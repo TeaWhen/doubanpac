8fa9ffae3d7bd6b86c251395b8e4f65e8195b6db
AquarHEAD L <aquarhead@gmail.com>
2013-04-14 15:30:02 +0800

    use douban style, fix permissions

--- doubanpac.js
+++ doubanpac.js
@@ -10,8 +10,32 @@ if (find_data.find.no_records > 0) {
 
   var pac_xml = new JKL.ParseXML( "http://webpac.zju.edu.cn/X?op=item-data&base=zju01&doc_number="+book_data.present.record.doc_number )
   var pac_data = pac_xml.parse();
+  console.log("doc_number in webpac is " + book_data.present.record.doc_number);
+
+  var webpac_info = $('<div class="gray_ad" id="webpac-info"><h2>浙大图书馆信息</h2><ul class="bs noline more-after"></ul></div>');
+  $('.gray_ad').first().before(webpac_info);
 
   for (n in pac_data["item-data"].item) {
-    $('#borrowinfo ul').append("<li style=\"border: none\">"+pac_data["item-data"].item[n]["sub-library"]+"</li>");
-  }
+    var item = pac_data["item-data"].item[n];
+    var status1 = "";
+    var status2 = "在架上";
+    if (item["item-status"] == "12") {
+      status1 = "普通外借";
+    } else {
+      status1 = "图书借阅";
+    }
+    if (item["loan-due-date"]) {
+      status1 = "已借出";
+      status2 = item["loan-due-date"];
+    }
+
+    var status = status1 + " | " + status2;
+    $('#webpac-info ul').append("<li style=\"border: none\">" + 
+      item["sub-library"] + 
+      " ( " +
+      status +
+      ") " +
+      item["call-no-1"] +
+      "</li>");
+  }  
 }
