87d24d2c39d4c9034e5d72ec024da4a2164c108d
AquarHEAD L <aquarhead@gmail.com>
2013-04-19 11:11:56 +0800

    use 10.10.16.94 if in ZJU

--- doubanpac.js
+++ doubanpac.js
@@ -1,77 +1,95 @@
-var book_isbn = /ISBN: (\d+)/.exec($("#info").text())[1];
-console.log("Book ISBN is "+ book_isbn);
-
-var find_xml = new JKL.ParseXML( "http://webpac.zju.edu.cn/X?op=find&base=zju01&code=wrd&request=" + book_isbn );
-var find_data = find_xml.parse();
-
-if (find_data.find.no_records > 0) {
-  var book_xml = new JKL.ParseXML( "http://webpac.zju.edu.cn/X?op=present&set_no="+find_data.find.set_number+"&set-entry=1" );
-  var book_data = book_xml.parse();
-
-  var pac_xml = new JKL.ParseXML( "http://webpac.zju.edu.cn/X?op=item-data&base=zju01&doc_number="+book_data.present.record.doc_number )
-  var pac_data = pac_xml.parse();
-  console.log("doc_number in webpac is " + book_data.present.record.doc_number);
-
-  var webpac_info = $('<div class="gray_ad" id="webpac-info"><h2>浙大图书馆信息</h2><ul class="bs noline more-after"></ul></div>');
-  $('.gray_ad').first().before(webpac_info);
-
-  for (n in pac_data["item-data"].item) {
-    var item = pac_data["item-data"].item[n];
-    var status1 = "";
-    var status2 = "";
-    if (item["item-status"] == "12") {
-      status1 = "普通外借";
-    } else {
-      status1 = "图书借阅";
-    }
-    if (item["loan-due-date"]) {
-      status1 = "已借出";
-      status2 = " | " +item["loan-due-date"];
-    }
-
-    var status = status1 + status2;
-    var book_info_str = item["sub-library"]+"｢"+status+"｣"+item["call-no-1"];
-    var send_link = " : <a href=\"#\" class=\"doubanpac-send\" data-title=\"" +
-      $('#wrapper h1 span').text() +
-      "\" data-content=\"" +
-      item["call-no-1"] + " " + item["sub-library"] +
-      "\"> ➾</a>";
-    $('#webpac-info ul').append("<li style=\"border: none\">" + 
-      book_info_str + send_link +
-      "</li>");
+$.ajax({
+  url: "http://10.10.16.94/X?op=find&base=zju01&code=wrd&request=python",
+  timeout: 1000,
+  success: function() {
+    console.log("in ZJU");
+    checkPac("http://10.10.16.94/");
+  },
+  error: function() {
+    console.log("out ZJU");
+    checkPac("http://webpac.zju.edu.cn/");
+  },
+  complete: function() {
+    console.log("load webpac complete");
   }
-}  
-
-function make_base_auth(username, password) {  
-  return "Basic " +Base64.encode(username + ':' + password);
-}  
-
-$('.doubanpac-send').click(function() {
-
-  var post_data = $(this).data();
-
-  
-  chrome.extension.sendMessage({method: "getServerInfo"}, function(response) {
-    if (response.url){
-      var remarqueurl = response.url + "/api/note/";
-      var remarqueusername = response.username;
-      var remarquepassword = response.password;
-      console.log(JSON.stringify(post_data));
-      $.ajax({
-        url: remarqueurl,
-        data: JSON.stringify(post_data),
-        type: 'POST',
-        contentType: 'text/plain; charset=UTF-8',
-        beforeSend : function(req) {  
-          req.setRequestHeader('Authorization', make_base_auth(remarqueusername, remarquepassword));  
-        }
-      });
-    } else {
-      // notify no settings
-    };
-  });
-  return false;
 });
+
+function checkPac(base_url) {
+  var book_isbn = /ISBN: (\d+)/.exec($("#info").text())[1];
+  console.log("Book ISBN is "+ book_isbn);
+
+  var find_xml = new JKL.ParseXML( base_url+"X?op=find&base=zju01&code=wrd&request=" + book_isbn );
+  var find_data = find_xml.parse();
+
+  if (find_data.find.no_records > 0) {
+    var book_xml = new JKL.ParseXML( base_url+"X?op=present&set_no="+find_data.find.set_number+"&set-entry=1" );
+    var book_data = book_xml.parse();
+
+    var pac_xml = new JKL.ParseXML( base_url+"X?op=item-data&base=zju01&doc_number="+book_data.present.record.doc_number )
+    var pac_data = pac_xml.parse();
+    console.log("doc_number in webpac is " + book_data.present.record.doc_number);
+
+    var webpac_info = $('<div class="gray_ad" id="webpac-info"><h2>浙大图书馆信息</h2><ul class="bs noline more-after"></ul></div>');
+    $('.gray_ad').first().before(webpac_info);
+
+    for (n in pac_data["item-data"].item) {
+      var item = pac_data["item-data"].item[n];
+      var status1 = "";
+      var status2 = "";
+      if (item["item-status"] == "12") {
+        status1 = "普通外借";
+      } else {
+        status1 = "图书借阅";
+      }
+      if (item["loan-due-date"]) {
+        status1 = "已借出";
+        status2 = " | " +item["loan-due-date"];
+      }
+
+      var status = status1 + status2;
+      var book_info_str = item["sub-library"]+"｢"+status+"｣"+item["call-no-1"];
+      var send_link = " : <a href=\"#\" class=\"doubanpac-send\" data-title=\"" +
+        $('#wrapper h1 span').text() +
+        "\" data-content=\"" +
+        item["call-no-1"] + " " + item["sub-library"] +
+        "\"> ➾</a>";
+      $('#webpac-info ul').append("<li style=\"border: none\">" + 
+        book_info_str + send_link +
+        "</li>");
+    }
+  }  
+
+  function make_base_auth(username, password) {  
+    return "Basic " +Base64.encode(username + ':' + password);
+  }  
+
+  $('.doubanpac-send').click(function() {
+
+    var post_data = $(this).data();
+
+    chrome.extension.sendMessage({method: "getServerInfo"}, function(response) {
+      if (response.url){
+        var remarqueurl = response.url + "/api/note/";
+        var remarqueusername = response.username;
+        var remarquepassword = response.password;
+        console.log("sending to Remarque");
+        $.ajax({
+          url: remarqueurl,
+          data: JSON.stringify(post_data),
+          type: 'POST',
+          contentType: 'text/plain; charset=UTF-8',
+          beforeSend : function(req) {  
+            req.setRequestHeader('Authorization', make_base_auth(remarqueusername, remarquepassword));  
+          }
+        });
+      } else {
+        alert("Please set Remarque settings.");
+      };
+    });
+    return false;
+  });
+
+}
 
 /* http://www.webtoolkit.info/javascript-base64.html */
 
